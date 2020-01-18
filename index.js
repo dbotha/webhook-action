5800af103a864bb79608eff5a3a1cc67b5b3e1d2
Deon Botha <deon@dbotha.com>
2020-01-18 22:22:55 +0000

    Revert "Initial commit"
    This reverts commit ab3fc124832fddc79e4e81f199d6398cedf4cbe4.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const exec = require("@actions/exec");
+const axios = require("axios");
 
 async function main() {
     try {
-        const output = await exec.exec("docker --version");
-        console.log(`Output: ${output}`);
+        const webhookURL = core.getInput("webhook-url");
+        const webhookPayload = core.getInput("webhook-payload");
+        const body = JSON.parse(webhookPayload);
+        console.log(`POSTing ${JSON.stringify(body)} to ${webhookURL}`);
+        await axios.post(webhookURL, body);
     } catch (error) {
         core.setFailed(error.message);
     }
